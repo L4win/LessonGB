cc8669d71fe7166fc0c36c0060082459eeb7ca58
Ruslan Abd <603540@bk.ru>
2021-12-09 14:21:23 +0500

    DZ4
    4

--- Less03_06.py
+++
@@ -1,9 +0,0 @@
-def int_func(text):
-    return text.title()
-
-
-words = []
-for word in input('Enter the word with a small letter: ').split():
-    words.append(int_func(word))
-
-print(f'It turns out the word: {" ".join(words)}')
--- Less03_3.py
+++
@@ -1,5 +0,0 @@
-def my_func(arg1, arg2, arg3):
-    print(f'The sum of the largest two arguments is: {arg1 + arg2 + arg3 - min([arg1, arg2, arg3])}')
-
-
-my_func(int(input('Enter Argument 1: ')), int(input('Enter Argument 2: ')), int(input('Enter Argument 3: ')))
